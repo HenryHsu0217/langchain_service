edd7a785597bf1565b1336cfd40ebb66084ab00b
henryhsu <henryhsu0217@gmail.com>
2024-07-09 10:54:16 +0800

    Change address,port to 0.0.0.0:8080

--- main.py
+++ main.py
@@ -272,4 +272,4 @@ async def query_model(request: QueryRequest):
     return {response["output"]}
 """
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
